872fe3b82d412a75e52d64120fbfee46ed7a259c
reza <hi95534reza@gmail.com>
2023-02-01 11:26:53 +0330

    settings login loguot signup forms ...

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,11 +5,11 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age',)
-
+        # fields = UserCreationForm.Meta.fields + ('age',)
+        fields = ('username', 'age', 'email', )
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
-
+        # fields = UserChangeForm.Meta.fields
+        fields = ('username', 'age', 'email',)
