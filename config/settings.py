098a826292f497cd2e55cc5802f169cbe7da57e6
reza <hi95534reza@gmail.com>
2023-02-01 13:41:03 +0330

    start the tests ...

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "accounts",
+
+
+    'accounts',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -123,7 +126,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'accounts.customUser'
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
